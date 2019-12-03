a65b9ba5c6ae508bcab7ec1073686b3ad2792553
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2019-12-03 16:40:42 +0000

    position change

--- js/maths.js
+++ js/maths.js
@@ -4,9 +4,9 @@ function distanceFromOrgin(position)
     y1 = position.y;
     z1 = position.z;
 
-    x2 = 0;
-    y2 = 0;
-    z2 = 0;
+    x2 = sun.position.x;
+    y2 = sun.position.y;
+    z2 = sun.position.z;
 
     distance = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2) + Math.pow(z2-z1,2));
     return distance;
